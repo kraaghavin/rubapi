e4c2aa150da0ab4f196d9389b29e9773deaf0b7e
kraaghav.in <kraaghav.in@gmail.com>
2023-05-01 01:09:45 +0530

    try3

--- config/env.js
+++ config/env.js
@@ -1,8 +1,8 @@
 export const PORT = process.env.PORT || 5001;
 export const MONGODB_URI = process.env.MONGODB_URI
 // 'mongodb://kraaghavin:NJ6FZLmrbmtQ8E5SS@ap-south-1.aws.realm.mongodb.com:27020/?authMechanism=PLAIN&authSource=%24external&ssl=true&appName=application-0-fclwp:mongodb-atlas:api-key' ;
-|| 'mongodb://kraaghavin:NJ6FZLmrbmtQ8E5SS@ap-south-1.aws.data.mongodb-api.com/app/application-0-fclwp/endpoint/data/v1'
-// ||  'mongodb+srv://kraaghavin:NJ6FZLmrbmtQ8E5S@cluster0.nqjkrmx.mongodb.net/test';
+// || 'mongodb://kraaghavin:NJ6FZLmrbmtQ8E5SS@ap-south-1.aws.data.mongodb-api.com/app/application-0-fclwp/endpoint/data/v1';
+||  'mongodb+srv://kraaghavin:NJ6FZLmrbmtQ8E5S@cluster0.nqjkrmx.mongodb.net/test-vercel';
 //    || 'mongodb://localhost:27017/custom-designs';
 export const JWT_SECRET = 'UzI1NiJ9.NjQzZDU5MTVhNmIx';
 // export const MNGO_SECRET = 'NJ6FZLmrbmtQ8E5S';
