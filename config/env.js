fae408b95e31bf60e441c6ef5718b0255958d2e8
kraaghav.in <kraaghav.in@gmail.com>
2023-05-01 00:55:57 +0530

    try2

--- config/env.js
+++ config/env.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 5001;
 export const MONGODB_URI = process.env.MONGODB_URI
 // 'mongodb://kraaghavin:NJ6FZLmrbmtQ8E5SS@ap-south-1.aws.realm.mongodb.com:27020/?authMechanism=PLAIN&authSource=%24external&ssl=true&appName=application-0-fclwp:mongodb-atlas:api-key' ;
-|| 'mongodb://ap-south-1.aws.data.mongodb-api.com/app/application-0-fclwp/endpoint/data/v1'
+|| 'mongodb://kraaghavin:NJ6FZLmrbmtQ8E5SS@ap-south-1.aws.data.mongodb-api.com/app/application-0-fclwp/endpoint/data/v1'
 // ||  'mongodb+srv://kraaghavin:NJ6FZLmrbmtQ8E5S@cluster0.nqjkrmx.mongodb.net/test';
 //    || 'mongodb://localhost:27017/custom-designs';
 export const JWT_SECRET = 'UzI1NiJ9.NjQzZDU5MTVhNmIx';
