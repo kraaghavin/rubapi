c0757f88d4ca2b571ca07fb4a707a3ef223e7447
Rubesh Gain <rubesh111@gmail.com>
2023-05-07 12:02:00 +0530

    Import Fix

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 import { connect } from 'mongoose';
-import { MONGODB_URI } from './env';
+import { MONGODB_URI } from './env.js';
 
 const connectDB = async () => {
   try {
     await connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
-      });
+    });
     console.log('MongoDB connected!');
   } catch (error) {
     console.error('MongoDB connection error', error);
--- routes/services.js
+++ routes/services.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 const serviceRoutes = Router();
-import { authenticateUser } from '../middleware/auth';
-import { createService, getServices, getServiceById, updateServiceById, deleteServiceById } from '../controllers/services';
+import { authenticateUser } from '../middleware/auth.js';
+import { createService, getServices, getServiceById, updateServiceById, deleteServiceById } from '../controllers/services.js';
 
 serviceRoutes.post('/', authenticateUser, createService);
 serviceRoutes.get('/', getServices);
